59fb3e1b4dbea6618c0937438171ee9b98a63c1f
Dmitry Ivanov <dmitry.ivanov.zagranju@gmail.com>
2017-01-12 11:45:09 +0100

    Corrections to Lista10

--- Lista10/task4.py
+++ Lista10/task4.py
@@ -12,7 +12,7 @@ amount_of_vowels = 0
 
 # calculate amount of vowels in text
 for letter in text:
-    if letter in vowels: 
+    if letter.lower() in vowels: 
         amount_of_vowels += 1
 
 # print results
--- Lista10/task5.py
+++ Lista10/task5.py
@@ -10,16 +10,19 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 # check if correct amount of arguments
 if len(argv) != 3:
     print ("Wrong amount of arguments.")
+
 # check if arguments are correct    
 elif argv[1][-4:] != ".txt":
-    print ("Wrong arguments.")
+    print ("Wrong file extension.")
 else:
     # add the file name to the directory path
     dir_path += "\\" + argv[1]
     
     # add a new line to the document
     with open(argv[1], 'a+') as file:
+        # if document exist - add on new line
         if os.path.isfile(dir_path) and os.path.getsize(dir_path) > 0:
             file.write("\n" + argv[2])
         else:
+            # if document doesn't exist - add first line
             file.write(argv[2])
