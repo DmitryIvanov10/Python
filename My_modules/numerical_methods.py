469afdc17e9d23a8a3fa74df94f4b065c8968607
Dmitry Ivanov <dmitry.ivanov.zagranju@gmail.com>
2017-01-15 19:01:56 +0100

    new module for numerical calculations

---
+++ My_modules/numerical_methods.py
@@ -0,0 +1,117 @@
+"""Module for numerical calculations"""
+
+def g(_f, a, b, c):
+    """Returns value of the function depending on a,b,c"""
+    return -( a*_f(b)*_f(c)*(_f(b)-_f(c)) + \
+              b*_f(c)*_f(a)*(_f(c)-_f(a)) + \
+              c*_f(a)*_f(b)*(_f(a)-_f(b)) ) / \
+            ( (_f(a)-_f(b)) * (_f(b)-_f(c)) * (_f(c)-_f(a)) )
+
+def bisection(_f, _a, _b, _eps = 0.0001):
+    """Finds zero place of function _f if possible in interval [_a, _b] 
+    with precision _eps by bisection method"""
+    """Returns zero place and number of iterations (x, iterations)""" 
+    # check if signs on two sides are different
+    if _f(_a) * _f(_b) > 0:
+        print ()
+        print ("No zero places.") 
+        print ()
+        return 0, -1
+
+    # iteration parameteres
+    iterations = 0
+
+    while True:
+        # calculate iteration
+        iterations += 1    
+
+        # devide interval into half
+        c = (_a + _b) / 2
+
+        # check if 0 on the division point
+        if abs(_f(c)) < _eps:
+            x = c
+            return x, iterations
+
+        # change sides depending on the value of the function in the middle of the interval
+        if _f(c) * _f(_a) > 0:
+            _a = c
+        else:
+            _b = c
+
+        if iterations > 1/_eps:
+            print ("No zero places.")
+            return 0, -1
+
+def brent(_f, _a, _b, _eps = 0.0001):
+    """Finds zero place of function _f if possible in interval [_a, _b] 
+    with precision _eps by Brent method""" 
+    """Returns zero place and number of iterations (x, iterations)"""
+    # check if signs on two sides are different
+    if _f(_a) * _f(_b) > 0:
+        print ()
+        print ("No zero places.") 
+        print ()
+        return 0, -1
+
+    # iteration parameteres
+    iterations = 0
+
+    while True:
+        # calculate iteration
+        iterations += 1    
+
+        # devide interval
+        if  iterations == 1 or x > _b or x < _a:
+            c = (_a + _b) / 2
+        elif abs(_f(c)) < _eps:
+            return x, iterations
+        else:
+            c = x 
+
+        # calculate possible x for Brent method (parabola from three points)
+        x = g(_f, _a, _b, c) 
+
+        # change sides depending on the value of the function in the middle of the interval
+        if _f(c) * _f(_a) < 0:
+            _b = c
+        else:
+            _a = c    
+
+def secant(_f, _a, _b, _eps = 0.0001):
+    """Finds zero place of function _f if possible in interval [_a, _b] 
+    with precision _eps by secant method""" 
+    """Returns zero place and number of iterations (x, iterations)"""
+
+    # iteration parameteres
+    iterations = 0
+
+    while True:
+        # calculate iteration
+        iterations += 1  
+
+        # find next element and reset previous
+        _a, _b = _b, _b - _f(_b) * (_b - _a) / (_f(_b) - _f(_a))
+
+        if abs(_f(_b)) < _eps:
+            return _b, iterations
+
+def newton(_f, _a, _eps = 0.0001):
+    """Finds zero place of function _f if possible from x0 = _a 
+    with precision _eps by Newton method""" 
+    """Returns zero place and number of iterations (x, iterations)"""
+
+    # iteration parameteres
+    iterations = 0
+
+    while True:
+        # calculate iteration
+        iterations += 1  
+
+        # find next element and reset previous
+        b = _a - _f(_a) * _eps / (_f(_a + _eps) - _f(_a))
+
+        if abs(b - _a) < _eps:
+            return b, iterations
+        else: 
+            _a = b
